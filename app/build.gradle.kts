6ef23c090764f1e4bd3d9dd9d9064d08e03ea9a1
mkx173 <zhw173@g.ucla.edu>
2026-04-16 15:07:51 +0900

    misc: update target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.mkx.hrttracker"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mkx.hrttracker"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
