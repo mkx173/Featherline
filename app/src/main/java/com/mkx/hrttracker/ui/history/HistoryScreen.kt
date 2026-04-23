d9b5735294f2e900412b7d1b1ceb539163debc68
mkx173 <zhw173@g.ucla.edu>
2026-04-23 17:23:42 +0900

    fix: scroll animation skip in calendar

--- app/src/main/java/com/mkx/hrttracker/ui/history/HistoryScreen.kt
+++ app/src/main/java/com/mkx/hrttracker/ui/history/HistoryScreen.kt
@@ -177,7 +177,7 @@ private fun HistoryScreenContent(
         firstDayOfWeek = uiState.calendarFirstDayOfWeek,
         outDateStyle = OutDateStyle.EndOfGrid
     )
-    val displayedMonth = rememberFirstMostVisibleMonth(calendarState, viewportPercent = 90f)
+    val displayedMonth = rememberFirstCompletelyVisibleMonth(calendarState)
 
     LaunchedEffect(displayedMonth.yearMonth) {
         onDisplayedMonthChange(displayedMonth.yearMonth)
@@ -1499,30 +1499,33 @@ private fun previewInstant(
 }
 
 @Composable
-private fun rememberFirstMostVisibleMonth(
-    state: CalendarState,
-    viewportPercent: Float = 50f,
-): CalendarMonth {
+fun rememberFirstCompletelyVisibleMonth(state: CalendarState): CalendarMonth {
     val visibleMonth = remember(state) { mutableStateOf(state.firstVisibleMonth) }
+    // Only take non-null values as null will be produced when the
+    // list is mid-scroll as no index will be completely visible.
     LaunchedEffect(state) {
-        snapshotFlow { state.layoutInfo.firstMostVisibleMonth(viewportPercent) }
+        snapshotFlow { state.layoutInfo.completelyVisibleMonths.firstOrNull() }
             .filterNotNull()
             .collect { month -> visibleMonth.value = month }
     }
     return visibleMonth.value
 }
 
-private fun CalendarLayoutInfo.firstMostVisibleMonth(viewportPercent: Float = 50f): CalendarMonth? {
-    return if (visibleMonthsInfo.isEmpty()) {
-        null
-    } else {
-        val viewportSize = (viewportEndOffset + viewportStartOffset) * viewportPercent / 100f
-        visibleMonthsInfo.firstOrNull { itemInfo ->
-            if (itemInfo.offset < 0) {
-                itemInfo.offset + itemInfo.size >= viewportSize
-            } else {
-                itemInfo.size - itemInfo.offset >= viewportSize
+private val CalendarLayoutInfo.completelyVisibleMonths: List<CalendarMonth>
+    get() {
+        val visibleItemsInfo = this.visibleMonthsInfo.toMutableList()
+        return if (visibleItemsInfo.isEmpty()) {
+            emptyList()
+        } else {
+            val lastItem = visibleItemsInfo.last()
+            val viewportSize = this.viewportEndOffset + this.viewportStartOffset
+            if (lastItem.offset + lastItem.size > viewportSize) {
+                visibleItemsInfo.removeAt(visibleItemsInfo.lastIndex)
             }
-        }?.month
+            val firstItem = visibleItemsInfo.firstOrNull()
+            if (firstItem != null && firstItem.offset < this.viewportStartOffset) {
+                visibleItemsInfo.removeAt(0)
+            }
+            visibleItemsInfo.map { it.month }
+        }
     }
-}
