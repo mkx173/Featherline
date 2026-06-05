1dd7fcdb5ff1bb96b6e471549222d4c36f6aa65b
mkx173 <zhw173@g.ucla.edu>
2026-06-05 16:53:47 +0900

    feat: support multi-medicine stock snackbars

--- app/src/test/java/com/mkx/hrttracker/ui/HrtTrackerAppTest.kt
+++ app/src/test/java/com/mkx/hrttracker/ui/HrtTrackerAppTest.kt
@@ -60,6 +60,42 @@ class HrtTrackerAppTest {
     }
 
     @Test
+    fun postLogStockWarningSnackbarMessage_manyUsesCountPluralForWorstState() {
+        every { context.resources } returns resources
+        every {
+            resources.getQuantityString(R.plurals.stock_toast_out_multiple, 3, 3)
+        } returns "3 medicines out of stock"
+        every {
+            resources.getQuantityString(R.plurals.stock_toast_imminent_multiple, 2, 2)
+        } returns "2 medicines almost out"
+        every {
+            resources.getQuantityString(R.plurals.stock_toast_user_low_multiple, 4, 4)
+        } returns "4 medicines low on stock"
+
+        assertEquals(
+            "3 medicines out of stock",
+            postLogStockWarningSnackbarMessage(
+                warning = PostLogStockWarning.Many(3, MedicineStockState.OUT),
+                context = context,
+            ),
+        )
+        assertEquals(
+            "2 medicines almost out",
+            postLogStockWarningSnackbarMessage(
+                warning = PostLogStockWarning.Many(2, MedicineStockState.IMMINENT),
+                context = context,
+            ),
+        )
+        assertEquals(
+            "4 medicines low on stock",
+            postLogStockWarningSnackbarMessage(
+                warning = PostLogStockWarning.Many(4, MedicineStockState.USER_LOW),
+                context = context,
+            ),
+        )
+    }
+
+    @Test
     fun postLogStockWarningDestination_singleDeepLinksToThatMedicineDetailUnderCurrentTab() {
         val uuid = UUID.fromString("11111111-2222-3333-4444-555555555555")
         val medicine = testMedicine(uuid = uuid)
@@ -76,6 +112,19 @@ class HrtTrackerAppTest {
                 medicineId = uuid.toString(),
                 topLevelParentRoute = Screen.Main.route,
             ),
+            destination,
+        )
+    }
+
+    @Test
+    fun postLogStockWarningDestination_manyRoutesToMedicinesListUnderCurrentTab() {
+        val destination = postLogStockWarningDestination(
+            PostLogStockWarning.Many(2, MedicineStockState.OUT),
+            topLevelParentRoute = Screen.Plan.route,
+        )
+
+        assertEquals(
+            Screen.Medicines.createRoute(topLevelParentRoute = Screen.Plan.route),
             destination,
         )
     }
